8d9d5c8dbe1c03f384437834e99d3a727109d6c3
Sergeyii <madzharovsc@gmail.com>
2018-07-02 11:46:32 +0300

    Enabled «pretty url`s»

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -36,14 +36,22 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
+            'class' => '\yii\web\UrlManager',
             'enablePrettyUrl' => true,
             'showScriptName' => false,
+            'enableStrictParsing' => true,
+            'baseUrl' => '',
+            'cache' => false,
             'rules' => [
+                '' => 'site/index',
+                '<_c:[\w\-]+>' => '<_c>/index',
+                '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
+                '<_c:[\w\-]+>/<_a:[\w-]+>' => '<_c>/<_a>',
+                '<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => '<_c>/<_a>',
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
